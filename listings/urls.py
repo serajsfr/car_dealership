d0e1c611d292816d296363537619369fa2ff9d2b
Seraj Safarian <70447169+serajsfr@users.noreply.github.com>
2024-07-13 12:48:57 +0330

    listing template created
    listing template for specific cars created

--- listings/urls.py
+++ listings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import listings
+from .views import listings, listing
 
 
 urlpatterns = [
     path('', listings, name='listings'),
+    path('listing/<id>/', listing, name='listing'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -7,5 +7,11 @@ def listings(request):
     context = {
         'listings': listings
     }
-    
     return render(request, 'listings.html', context)
+
+def listing(request, id):
+    listing = Listing.objects.get(id=id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listing.html', context)
