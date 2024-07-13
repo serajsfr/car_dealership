3ea189998499329448c9ba554a8cd94a53984d41
Seraj Safarian <70447169+serajsfr@users.noreply.github.com>
2024-07-13 14:51:01 +0330

    Update create and update
    create and update templates are updated

--- listings/urls.py
+++ listings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import listings, listing, create
+from .views import listings, listing, create, update
 
 
 urlpatterns = [
     path('', listings, name='listings'),
     path('listing/<id>/', listing, name='listing'),
     path('create/', create, name='create'),
+    path('update/<id>/', update, name='update'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Listing
 from .forms import ListingForm
 
@@ -19,7 +19,25 @@ def listing(request, id):
 
 def create(request):
     form = ListingForm
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid:
+            form.save()
+        return redirect('/')
     context = {
         'form': form
     }
     return render(request, 'create.html', context)
+
+def update(request, id):
+    listing = Listing.objects.get(id=id)
+    form = ListingForm(instance=listing)
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid:
+            form.save()
+        return redirect('/')
+    context = {
+        'form': form
+    }
+    return render(request, 'update.html', context)
