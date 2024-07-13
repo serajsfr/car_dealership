b3d1210ce5a2391455de020ce1d2676bdf5e3dfb
Seraj Safarian <70447169+serajsfr@users.noreply.github.com>
2024-07-13 12:17:09 +0330

    models created
    Listing model created

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# name
+# description
+# brand
+# milage
+# price
+# image
+class Listing(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    brand = models.CharField(max_length=100)
+    milage = models.IntegerField()
+    price = models.IntegerField()
+    # image
